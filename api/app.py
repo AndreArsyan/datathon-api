95853ed2f52c1ad049de738b2e2384c46b8dd93f
Andre Arsyan <andrearsyanj@gmail.com>
2023-11-03 18:18:29 -0400

    add test

--- api/app.py
+++ api/app.py
@@ -5,30 +5,33 @@ from flask import request
 from flask_cors import CORS, cross_origin
 from flask import jsonify
 
-# from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 import joblib
-# from sklearn.datasets import fetch_california_housing
-# from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
 # from sklearn.linear_model import LinearRegression
 # import pickle
 import numpy as np
 
+from app2 import app2_api
+
 app = Flask(__name__)
+app.register_blueprint(app2_api)
 CORS(app)
 #
-# # Load the dataset
-# california = fetch_california_housing()
-# X = california.data
-# y = california.target
-# #
-# # # Split the data into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# #
-# # # Standardize the features
-# scaler = StandardScaler()
-# X_train_scaled = scaler.fit_transform(X_train)
-# X_test_scaled = scaler.transform(X_test)
+# Load the dataset
+california = fetch_california_housing()
+X = california.data
+y = california.target
 #
+# # Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#
+# # Standardize the features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
 # # Load the trained model
 model = joblib.load('california_house_price_model.pkl')
 
@@ -50,11 +53,10 @@ def predict_price():
                           float(request.args.get('Longitude'))]
 
         # Scale the user inputs using the same scaler used during training
-        # scaled_features = scaler.transform([input_features])
+        scaled_features = scaler.transform([input_features])
 
         # Make a prediction using the model
-        predicted_price = model.predict([np.array(input_features)])[0]
-        # predicted_price = model.predict(scaled_features)[0]
+        predicted_price = model.predict(scaled_features)[0]
 
 
         return jsonify(predicted_price)
