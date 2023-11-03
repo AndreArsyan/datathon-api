3961b6eb4efa75cd04f446a18f8537ea42da4e45
Andre Arsyan <andrearsyanj@gmail.com>
2023-11-03 19:28:26 -0400

    code cleanup

--- api/app.py
+++ api/app.py
@@ -1,37 +1,16 @@
-from flask import Flask
-from flask import request
-# import requests
-# import json
-from flask_cors import CORS, cross_origin
-from flask import jsonify
+# API
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-from sklearn.preprocessing import StandardScaler
+# ML
 import joblib
-from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression
-# import pickle
-
-# from app2 import app2_api
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
-# app.register_blueprint(app2_api)
 CORS(app)
-#
-# Load the dataset
-# california = fetch_california_housing()
-# X = california.data
-# y = california.target
 
-# print("x:",X,"y:",y)
-#
-# # Split the data into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#
 # # Standardize the features
 scaler = StandardScaler()
-# print("x:",X_train,"y:",X_test)
-
 X_train_scaled = scaler.fit_transform([[1,2,3,4,5,6,7,8]])
 X_test_scaled = scaler.transform([[1,2,3,4,5,6,7,8]])
 
@@ -42,29 +21,25 @@ model = joblib.load('california_house_price_model.pkl')
 def home():
     return "HTTP 200 OK"
 
-@app.route('/api', methods=['GET', 'POST'])
+@app.route('/api', methods=['GET'])
 def predict_price():
-    if request.method == 'GET':
-        # Get user inputs for the features
-        input_features = [float(request.args.get('MedInc')),
-                          float(request.args.get('HouseAge')),
-                          float(request.args.get('AveRooms')),
-                          float(request.args.get('AveBedrms')),
-                          float(request.args.get('Population')),
-                          float(request.args.get('AveOccup')),
-                          float(request.args.get('Latitude')),
-                          float(request.args.get('Longitude'))]
+    # Get user inputs for the features
+    input_features = [float(request.args.get('MedInc')),
+                      float(request.args.get('HouseAge')),
+                      float(request.args.get('AveRooms')),
+                      float(request.args.get('AveBedrms')),
+                      float(request.args.get('Population')),
+                      float(request.args.get('AveOccup')),
+                      float(request.args.get('Latitude')),
+                      float(request.args.get('Longitude'))]
 
-        # Scale the user inputs using the same scaler used during training
-        scaled_features = scaler.transform([input_features])
+    # Scale the user inputs using the same scaler used during training
+    scaled_features = scaler.transform([input_features])
 
-        # Make a prediction using the model
-        predicted_price = model.predict(scaled_features)[0]
+    # Make a prediction using the model
+    predicted_price = model.predict(scaled_features)[0]
 
-
-        return jsonify(predicted_price)
-
-        # return jsonify(0)
+    return jsonify(predicted_price)
 
 
 if __name__ == '__main__':
